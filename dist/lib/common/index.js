e53df83ac5bd9e14ea3a5d3dc9a699d94edc8211
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-04-17 15:02:01 +0200

    chore(remove dist folder):

--- dist/lib/common/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var IIndex_1 = require("./interfaces/IIndex");
-exports.IIndex = IIndex_1.IIndex;
-var IObject_1 = require("./interfaces/IObject");
-exports.IObject = IObject_1.IObject;
-var api_1 = require("./urls/api");
-exports.apiUrls = api_1.apiUrls;
--- dist/lib/common/utils/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.measurementSort = (a, b) => {
-    return new Date(a.date) - new Date(b.date);
-};
