9c9ff26ccc19d7648b125d4caa6aa5afc3e47ac6
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-04-23 18:54:42 +0200

    feat(icons trafficlight component): Adds a component that creates icons

---
+++ src/lib/views/components/questionnaire/quest-traffic-light.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TrafficLightColorNames } from '../../../common/enums';
+
+interface IQuestTrafficLightIcon {
+  colorText: string;
+}
+export const QuestTrafficLightIcon = (props: IQuestTrafficLightIcon) => {
+  const defaultClassName = 'icon__traffic-light';
+  if (props.colorText === TrafficLightColorNames.green) {
+    return (
+      <span className={`${defaultClassName} ${defaultClassName}--green`}></span>
+    );
+  } else if (props.colorText === TrafficLightColorNames.orange) {
+    return (
+      <span className={`${defaultClassName} ${defaultClassName}--orange`}></span>
+    );
+  } else if (props.colorText === TrafficLightColorNames.red) {
+    return (
+      <span className={`${defaultClassName} ${defaultClassName}--red`}></span>
+    );
+  } else {
+    return (<span className={`${defaultClassName}`}></span>);
+  }
+};
