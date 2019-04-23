e709e3a016f619ab68b3c8a10112db1b2a20c98f
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-04-23 18:56:26 +0200

    feat(traffic light icon): Adds the call of traffic light to question layout

--- src/lib/views/pages/question.tsx
+++ src/lib/views/pages/question.tsx
@@ -7,12 +7,17 @@
 
 import React from 'react';
 
+import { TrafficLightColorNames } from '../../common/enums';
 import { IObject } from '../../common/interfaces';
+import { QuestNav } from '../components/questionnaire/quest-nav';
+import { QuestTrafficLightIcon } from '../components/questionnaire/quest-traffic-light';
 import { Skeleton } from '../layouts/skeleton';
 
 interface IQuestion {
   [key: string]: any;
   data: IObject;
+  qId: number;
+  lastId: number;
 }
 
 const questionnaire = (props: IQuestion) => {
@@ -30,40 +35,95 @@ const questionnaire = (props: IQuestion) => {
   // const umsetzungswahrscheinlichkeit: string | null = data[1][10];
   // const infrastrukturOderVerhandlung: string | null = data[1][11];
   // console.log('data in question', data);
-  const answers: string[] = [];
+
+  interface IAnswer {
+    text: string;
+    colorText: string;
+  }
+
+  const answers: IAnswer[] = [];
   for (let i = 1; i < data.length; i++) {
     if (data[i][6] !== null) {
-      answers.push(data[i][6]);
+      const datum: IAnswer = {
+        colorText: data[i][9],
+        text: data[i][6],
+      };
+      answers.push(datum);
     }
   }
-  console.log(answers);
+  // console.log(answers);
   return (
 
-    <Skeleton title={'Foo'}>
-      <div className='question__body'>
+    <Skeleton title={`Standortbewertung Frage ${props.qId} von ${props.lastId + 1}`}>
+      <div className='questions__body section'>
         <h1>{data[1][1]}</h1>
-        <p className='question__body-general-info'>
-        {data[1][3]}
+        <p className='questions__body-general-info'>
+          {data[1][3]}
         </p>
-        <form style={{width: '100%', float: 'left'}}>
+        <div className='questions__body-nav columns'>
+          <div className='column'>
+            <QuestNav
+              next={props.qId === props.lastId ? undefined : props.qId + 1}
+              previous={props.qId - 1 < 0 ? undefined : props.qId - 1}
+            />
+          </div>
+        </div>
+        <div className='questions__body-progress'>
+          <div className='column'>
+            <progress className='progress'
+              value={`${props.qId + 1}`}
+              max={`${props.lastId + 1}`}
+            >{`${props.qId + 1} von ${props.lastId + 1}`}</progress>
+          </div>
+        </div>
+        <h2 className='questions__body-question-head columns'>Frage:</h2>
+        <div className='questions__body-question columns'>
+          <div className='column'>
+            <p>
+              {data[1][4]}
+            </p>
+          </div>
+          {/* <div className='column'>
+            {(() => {
+              const defaultClassName = 'questions__body-question-tlight';
+              if (data[1][9] === TrafficLightColorNames.green) {
+                return (<div className={`${defaultClassName}--green`}>{data[1][9]}</div>);
+              } else if (data[1][9] === TrafficLightColorNames.orange) {
+                return (<div className={`${defaultClassName}--orange`}>{data[1][9]}</div>);
+              } else if (data[1][9] === TrafficLightColorNames.red) {
+                return (<div className={`${defaultClassName}--red`}>{data[1][9]}</div>);
+              } else {
+                return (<div className={`${defaultClassName}`}>{data[1][9]}</div>);
+              }
+            })()
+            }
+          </div> */}
+        </div>
+        <hr />
+        <form className='questions__form columns'>
+          <div className='column'>
             <div className='control'>
-            {answers.map((answer: string, i: number) => {
-              return (
-              <div className='field is-fullwidth'>
-                <label htmlFor={`answer_${i}`} className='radio' key={i} >
-                  <input type='radio' className='is-fullwidth' id={`answer_${i}`} name='answer'/>
-              {answer}
-              </label>
-              </div>
-            );
-            })}{(() => {
-              return <br />;
+              {answers.map((answer: IAnswer, i: number) => {
+                return (
+                  <div className='field is-fullwidth'>
+                    <QuestTrafficLightIcon
+                      colorText={answer.colorText} />
+                    <label htmlFor={`answer_${i}`} className='radio' key={i} >
+                      <input type='radio' className='is-fullwidth' id={`answer_${i}`} name='answer' />
+                      {answer.text}
+                    </label>
+                  </div>
+                );
+              })}
+              {(() => {
+                return <br />;
               })()}
             </div>
+          </div>
         </form>
-        <hr/>
-        <p className='question__body-additional-info'>
-        {data[1][5]}
+        <hr />
+        <p className='questions__body-additional-info columns'>
+          {data[1][5]}
         </p>
       </div>
       <table>
